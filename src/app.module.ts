f64454958846fa6bee4e8236a1fe9e7965724ba0
MNihal7961 <mnihal7961@gmail.com>
2026-09-03 17:40:46 +0530

    Add AppController and AppService to AppModule; configure global validation and Swagger documentation in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { databaseConfig } from './config/database.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
@@ -15,5 +17,7 @@ import { UserModule } from './user/user.module';
     AuthModule,
     UserModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
