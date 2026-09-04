befd77307254951705633e95beab4d85f0823f97
MNihal7961 <mnihal7961@gmail.com>
2026-09-04 13:13:01 +0530

    Refactor main.ts to configure application settings in a separate function

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configureApp } from './create-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    methods: '*',
-    allowedHeaders: '*',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Talent Lens API')
-    .setDescription('API documentation for the Talent Lens backend')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document);
-
+  configureApp(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
