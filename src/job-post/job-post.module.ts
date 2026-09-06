a7e75c97509adbdf8f0e6c0eec60a8ab31bd45fe
MNihal7961 <mnihal7961@gmail.com>
2026-09-06 19:29:14 +0530

    Implement JobPostModule with Mongoose integration, controllers, and services

--- src/job-post/job-post.module.ts
+++ src/job-post/job-post.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JobPost, JobPostSchema } from './job-post.model';
+import { JobPostService } from './job-post.service';
+import { JobPostController } from './job-post.controller';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: JobPost.name, schema: JobPostSchema }]),
+  ],
+  controllers: [JobPostController],
+  providers: [JobPostService],
+  exports: [JobPostService],
+})
 export class JobPostModule {}
